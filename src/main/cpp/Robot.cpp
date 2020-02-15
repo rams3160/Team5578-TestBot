af060eafae0666a8fb3df910f301db4e5d3ae5f3
Billy R <bill.e.ramsey@gmail.com>
2020-02-15 13:52:30 -0600

    Initial Commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,95 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+
+#include <iostream>
+
+#include <ctre/phoenix/motorcontrol/can/WPI_TalonSRX.h>
+#include <frc/SpeedControllerGroup.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/Joystick.h>
+#include <ctre/Phoenix.h>
+
+
+
+WPI_TalonSRX m_talon1{1};
+WPI_TalonSRX m_talon2{2};
+
+frc::DifferentialDrive m_drive{m_talon1,m_talon2};
+frc::Joystick m_driverController{0};
+
+void Robot::RobotInit() {
+  m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
+  m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
+  frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  
+  m_driverController.SetYChannel(1);
+  m_driverController.SetXChannel(4);
+
+}
+
+/**
+ * This function is called every robot packet, no matter the mode. Use
+ * this for items like diagnostics that you want ran during disabled,
+ * autonomous, teleoperated and test.
+ *
+ * <p> This runs after the mode specific periodic functions, but before
+ * LiveWindow and SmartDashboard integrated updating.
+ */
+void Robot::RobotPeriodic() {
+
+
+}
+
+/**
+ * This autonomous (along with the chooser code above) shows how to select
+ * between different autonomous modes using the dashboard. The sendable chooser
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+ * remove all of the chooser code and uncomment the GetString line to get the
+ * auto name from the text box below the Gyro.
+ *
+ * You can add additional auto modes by adding additional comparisons to the
+ * if-else structure below with additional strings. If using the SendableChooser
+ * make sure to add them to the chooser code above as well.
+ */
+void Robot::AutonomousInit() {
+  m_autoSelected = m_chooser.GetSelected();
+  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
+  //     kAutoNameDefault);
+  std::cout << "Auto selected: " << m_autoSelected << std::endl;
+
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::AutonomousPeriodic() {
+  if (m_autoSelected == kAutoNameCustom) {
+    // Custom Auto goes here
+  } else {
+    // Default Auto goes here
+  }
+}
+
+void Robot::TeleopInit() {
+  m_talon2.SetInverted(false);
+  m_talon2.SetInverted(false);
+}
+
+void Robot::TeleopPeriodic() {
+m_drive.ArcadeDrive(m_driverController.GetX(),m_driverController.GetY());
+}
+
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main() { return frc::StartRobot<Robot>(); }
+#endif
